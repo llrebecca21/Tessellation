db6fe40d7c720d4cfe17d737611b66605290ace7
llrebecca21 <llrebecca21@gmail.com>
2023-01-19 16:54:33 -0600

    Fix dimension error in creating x_t

--- R/main_11-19.R
+++ R/main_11-19.R
@@ -10,19 +10,19 @@ library(NPflow)
 library(plotly)
 library(invgamma)
 library(gtools)
-library(forecast)
+#library(forecast)
 library(RcppArmadillo)
 
 source('R/M_lprior.R')
 source('R/tessellation_likelihood.R')
 source('R/time_interval.R')
 # Get updated file!
-source('R/MHstep_BFGS_11-19.R')
+#source('R/MHstep_BFGS_11-19.R')
 source('R/effective_partition.R')
 # Error
-Rcpp::sourceCpp('src/distance_partitionC_Lee.cpp')
+#Rcpp::sourceCpp('src/distance_partitionC_Lee.cpp')
 # Error
-Rcpp::sourceCpp('src/distance_partition.cpp')
+#Rcpp::sourceCpp('src/distance_partition.cpp')
 
 
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
@@ -65,6 +65,7 @@ xx[20,] = c(0.8,0.8)
 
 # Redo code to create column names #
 x = matrix(data = 0, nrow = Ntime*NumObs, ncol = NumXT+2)
+nrow(x) # 20_000
 # Create column names for x1 and x2
 xname = paste("x", 1:(NumX), sep = "")
 # Create name vector
@@ -76,54 +77,59 @@ colnames(x) = names
 
 # Fill in the columns of x #
 # replace index with 1:20 with each repeated 1_000 times
-x$index=rep(1:NumObs,each=Ntime)
+x$index = rep(1:NumObs,each=Ntime)
 # replace time with 1:1000 repeated 20 times
-x$time=rep(1:Ntime,NumObs)
+x$time = rep(1:Ntime,NumObs)
 # replace scaled_time with proportion of length completed; repeats each 1_000 index  
-x$scaled_time=range01(x$time)
+x$scaled_time = range01(x$time)
 
 
 # Repeat each row of xx 1000 times 
-x[,4:(NumXT+2)]=apply(X = xx, MARGIN = 2, FUN = function(i) rep(i,each=Ntime))
+x[,4:(NumXT+2)] = apply(X = xx, MARGIN = 2, FUN = function(i) rep(i,each=Ntime))
 
 
 ## create time series x_t, abrupt change at x2, and at half time points
 ## Redo ts.sim, ts.sim1, ts.sim2 ##
-# ts.sim = matrix(data = 0, nrow = NumObs, ncol = Ntime)
-ts.sim1 = matrix(data = 0, nrow = Ntime/2, ncol = NumObs)
-ts.sim2 = matrix(data = 0, nrow = Ntime/2, ncol = NumObs)
+ts.sim = matrix(data = 0, nrow = NumObs, ncol = Ntime)
+ts.sim1 = matrix(data = 0, nrow = NumObs, ncol = Ntime/2)
+ts.sim2 = matrix(data = 0, nrow = NumObs, ncol = Ntime/2)
+#length(ts.sim2) #10000
 
+#ts.sim=matrix(rep(0,NumObs*Ntime),NumObs,Ntime)
+#ts.sim1=matrix(rep(0,NumObs*Ntime/2),NumObs,Ntime/2)
+#ts.sim2=matrix(rep(0,NumObs*Ntime/2),NumObs,Ntime/2)
+length(ts.sim2) #10000
 
-for(i in 1:NumObs)
+for(l in 1:NumObs)
 {
-  if(xx[i,1]<0.5)
+  if(xx[l,1]<0.5)
   {
     
-    if(xx[i,2]<0.5)
+    if(xx[l,2]<0.5)
     {
-      ts.sim1[i,]=arima.sim(list(order=c(1,0,0),ar=0.3),n=Ntime/2)
-      ts.sim2[i,]=arima.sim(list(order=c(1,0,0),ar=-0.5),n=Ntime/2)
+      ts.sim1[l,]=arima.sim(list(order=c(1,0,0),ar=0.3),n=Ntime/2)
+      ts.sim2[l,]=arima.sim(list(order=c(1,0,0),ar=-0.5),n=Ntime/2)
       
     }else{
       
-      ts.sim1[i,]=arima.sim(list(order=c(1,0,0),ar=-0.9),n=Ntime/2)
-      ts.sim2[i,]=arima.sim(list(order=c(1,0,0),ar=0.7),n=Ntime/2)
+      ts.sim1[l,]=arima.sim(list(order=c(1,0,0),ar=-0.9),n=Ntime/2)
+      ts.sim2[l,]=arima.sim(list(order=c(1,0,0),ar=0.7),n=Ntime/2)
       
     }
     
     
   }else{
     
-    if(xx[i,2]<0.5)
+    if(xx[l,2]<0.5)
     {
       
-      ts.sim1[i,]=arima.sim(list(order=c(1,0,0),ar=-0.3),n=Ntime/2)
-      ts.sim2[i,]=arima.sim(list(order=c(1,0,0),ar=0.5),n=Ntime/2)
+      ts.sim1[l,]=arima.sim(list(order=c(1,0,0),ar=-0.3),n=Ntime/2)
+      ts.sim2[l,]=arima.sim(list(order=c(1,0,0),ar=0.5),n=Ntime/2)
       
     }else{
       
-      ts.sim1[i,]=arima.sim(list(order=c(1,0,0),ar=0.9),n=Ntime/2)
-      ts.sim2[i,]=arima.sim(list(order=c(1,0,0),ar=-0.7),n=Ntime/2)
+      ts.sim1[l,]=arima.sim(list(order=c(1,0,0),ar=0.9),n=Ntime/2)
+      ts.sim2[l,]=arima.sim(list(order=c(1,0,0),ar=-0.7),n=Ntime/2)
       
     }
     
@@ -131,7 +137,7 @@ for(i in 1:NumObs)
 }
 
 
-x_t = rbind(ts.sim1,ts.sim2)  
+x_t = t(cbind(ts.sim1,ts.sim2))  
 
 # Redo Standardized #
 xmat = cbind(1,1:Ntime)
