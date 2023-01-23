c65b5c845bd1d147d849bf39c88f751e138d9e22
llrebecca21 <llrebecca21@gmail.com>
2023-01-23 13:23:13 -0600

    Add comments to check the covariates for the for loop when creating the time series.

--- R/main_11-19.R
+++ R/main_11-19.R
@@ -11,7 +11,7 @@ library(plotly)
 library(invgamma)
 library(gtools)
 library(styler)
-#library(forecast)
+library(forecast)
 library(RcppArmadillo)
 
 source('R/M_lprior.R')
@@ -31,6 +31,7 @@ range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 ##################
 # Code Directory
 ##################
+
 # Abrupt-abrupt simulate data : lines ...
 # Slowly-abrupt simulate data : lines ...
 # Hyperparameters for tessellation partition : lines ...
@@ -41,41 +42,45 @@ range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
 ###################################
 # Abrupt-abrupt simulate data
-#################################
+###################################
 set.seed(1080)
 # Define variables
 Ntime = 1000
 NumX = 2
-NumXT = NumX+1
+NumXT = NumX + 1
 NumObs = 20
 
-## create covariates matrix of x
+## create covariate matrix of x
+## xx : (20 x 2) matrix
 xx = matrix(0,NumObs,NumX)
 
 
-## True S is 2,8,13,18
-xx[1,] = c(0.2,0.4)
-xx[2,] = c(0.3,0.3)
-xx[3,] = c(0.4,0.2)
-xx[4,] = c(0.2,0.2)
-xx[5,] = c(0.4,0.4)
-xx[6,] = c(0.2,0.6)  
-xx[7,] = c(0.2,0.8)
-xx[8,] = c(0.3,0.7)  
-xx[9,] = c(0.4,0.6)  
-xx[10,] = c(0.4,0.8)  
-xx[11,] = c(0.6,0.2)  
-xx[12,] = c(0.6,0.4)  
-xx[13,] = c(0.7,0.3)
-xx[14,] = c(0.8,0.2)
-xx[15,] = c(0.8,0.4)
-xx[16,] = c(0.6,0.6)
-xx[17,] = c(0.6,0.8)
-xx[18,] = c(0.7,0.7)
-xx[19,] = c(0.8,0.6)
-xx[20,] = c(0.8,0.8)
+## True S (centers) is 2,8,13,18
+xx[1,] = c(0.2,0.4) # both below
+xx[2,] = c(0.3,0.3) # both below
+xx[3,] = c(0.4,0.2) # both below
+xx[4,] = c(0.2,0.2) # both below
+xx[5,] = c(0.4,0.4) # both below
+xx[6,] = c(0.2,0.6) # first below, second above
+xx[7,] = c(0.2,0.8) # first below, second above
+xx[8,] = c(0.3,0.7) # first below, second above 
+xx[9,] = c(0.4,0.6) # first below, second above 
+xx[10,] = c(0.4,0.8) # first below, second above  
+xx[11,] = c(0.6,0.2) # first above, second below  
+xx[12,] = c(0.6,0.4) # first above, second below  
+xx[13,] = c(0.7,0.3) # first above, second below
+xx[14,] = c(0.8,0.2) # first above, second below
+xx[15,] = c(0.8,0.4) # first above, second below
+xx[16,] = c(0.6,0.6) # first above, second above
+xx[17,] = c(0.6,0.8) # first above, second above
+xx[18,] = c(0.7,0.7) # first above, second above
+xx[19,] = c(0.8,0.6) # first above, second above
+xx[20,] = c(0.8,0.8) # first above, second above
 
-# Redo code to create column names #
+plot(xx, main = "Plot of Covariances")
+
+
+# Redo code to create column names # 20_000 x 5
 x = matrix(data = 0, nrow = Ntime*NumObs, ncol = NumXT+2)
 nrow(x) # 20_000
 # Create column names for x1 and x2
@@ -89,15 +94,15 @@ colnames(x) = names
 
 # Fill in the columns of x #
 # replace index with 1:20 with each repeated 1_000 times
-x$index = rep(1:NumObs,each=Ntime)
+x$index = rep(1:NumObs, each = Ntime)
 # replace time with 1:1000 repeated 20 times
-x$time = rep(1:Ntime,NumObs)
+x$time = rep(1:Ntime, NumObs)
 # replace scaled_time with proportion of length completed; repeats each 1_000 index  
 x$scaled_time = range01(x$time)
 
 
 # Repeat each row of xx 1000 times 
-x[,4:(NumXT+2)] = apply(X = xx, MARGIN = 2, FUN = function(i) rep(i,each=Ntime))
+x[,4:(NumXT+2)] = apply(X = xx, MARGIN = 2, FUN = function(i) rep(i,each = Ntime))
 
 
 ## create time series x_t, abrupt change at x2, and at half time points
@@ -141,6 +146,8 @@ for(i in 1:NumObs)
   }
 }
 
+#ts.plot(arima.sim(list(order = c(1,0,0), ar = -0.7), n = Ntime/2))
+
 # column bind and transpose the resulting two time series created above
 x_t = t(cbind(ts.sim1, ts.sim2))  
 
@@ -149,9 +156,14 @@ xmat = cbind(1,1:Ntime)
 linfit = solve(crossprod(xmat), crossprod(xmat,x_t))
 x_t = x_t - xmat %*% linfit
 
+# Plot the 20th time series simulated to check
+# All plots are in abrupt_abrupt_simplots.R
 ts.plot(x_t[,20])
 
-plot(x[,c(4,5)])
+
+# plot(x[,c(4,5)])
+
+
 # Code output matches Yakun's up until this point
 
 ###################################
@@ -282,6 +294,7 @@ x_t <- x_t - xmat %*% linfit
 
 # plot the time series
 ts.plot(x_t[, 20])
+ts.plot(x_t[, 6])
 
 # plot portion of x
 plot(x[, c(4, 5)])
