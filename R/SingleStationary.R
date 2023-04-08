a07fc2274dcec02b86dc19e614d2fef12259ebe1
llrebecca21 <llrebecca21@gmail.com>
2023-04-08 15:31:31 -0500

    Create Hyperparameters for MCMC and priors. Create matrices to store outputs from MCMC algorithm.

--- R/SingleStationary.R
+++ R/SingleStationary.R
@@ -16,6 +16,46 @@ phi <- 0.5
 # Model 1 : AR(1) with phi = 0.5
 ts1 <- arima.sim(model = list("ar" = phi), n = n, n.start = burn)
 
+#################
+# MCMC parameters
+#################
+
+# number of basis functions/number of beta values
+K <- 10
+# nbeta stores number of beta values (beta_{1:K}) + intercept term (alpha_0)
+alphabeta <- K + 1
+# prior intercept variance (variance associated with the alpha_0 prior)
+sigmasalpha <- 100
+# maximum value for tau^2 (Indicator in paper).
+maxtau <- 1000
+# Define omega for the Basis Functions
+omega <- (0:(n-1)) / n
+
+# Set number of iterations
+iter <- 100
+  
+#######################
+# Initialize parameters
+#######################
+# set tau^2 value
+tausquared <- 10
+# set intercept term
+alpha0 <- 0
+# set beta
+betavalues <- rep(0, K)
+
+# Create matrix to store samples
+# ncol: number of parameters (alpha0, Beta, tau^2)
+Theta <- matrix(NA, nrow = iter, ncol = K + 2)
+
+# Create matrix of the basis functions
+# unscale by n (i.e. 2/n)
+# fix fourier frequencies by multiplying by 2 (inside cosine function)
+X <- outer(X = omega, Y = 1:K, FUN = function(x,y){sqrt(2/n)*cos(2 * pi * y * x)})
+
+# X is orthonormal
+round(crossprod(X),5)
+
 
 
 
