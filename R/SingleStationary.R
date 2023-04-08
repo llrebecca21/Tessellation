93740459f4be391012902fc05a27d45e7344424b
llrebecca21 <llrebecca21@gmail.com>
2023-04-08 14:36:41 -0500

    Create hyperparameters and data for AR(1) sim.

--- R/SingleStationary.R
+++ R/SingleStationary.R
@@ -1,1 +1,23 @@
 # Single Stationary Time Series
+
+set.seed(1080)
+
+# Create single time series: AR(1)
+
+# set hyper-parameters
+
+# length of time series
+n <- 1000
+# burn-in period
+burn <- 50
+# Create coefficient phi
+phi <- 0.5
+
+# Model 1 : AR(1) with phi = 0.5
+ts1 <- arima.sim(model = list("ar" = phi), n = n, n.start = burn)
+
+
+
+
+
+
