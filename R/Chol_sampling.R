a1388293444b34621881040e211f0deb4fa33e5c
llrebecca21 <llrebecca21@gmail.com>
2023-04-10 15:42:18 -0500

    Update Cholesky Sampling Function to simplify code and clarify input values. Insert Roxygen skeleton and update documentation for the function.

--- R/Chol_sampling.R
+++ R/Chol_sampling.R
@@ -1,12 +1,16 @@
-###### Cholesky sampling
-Chol_sampling=function(Q,d,mean)
-{
-  L=t(chol(Q))   ## L : left Cholesky factor
-  Lt=chol(Q)    ## right Cholesky factor
-  z=t(rmvnorm(1,rep(0,d),diag(d)))
-  u=solve(Lt,z)
-  beta_prop=mean+u
-  
+#' Chol_sampling
+#'
+#' @param Lt : (nbeta x nbeta) : Upper triangular matrix from Cholesky decomposition
+#' @param d  : (numeric : nbeta) : Length of beta
+#' @param beta_c : (nbeta x 1) : Vector of current beta values
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Chol_sampling <- function(Lt, d, beta_c) {
+  u <- solve(Lt, rnorm(n = d))
+  beta_prop <- beta_c + u
+
   return(beta_prop)
-  
 }
