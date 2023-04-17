1f5e5213b4c30f1677c1fb238c0de62417af21af
llrebecca21 <llrebecca21@gmail.com>
2023-04-17 10:43:54 -0500

    Update the inputs to the whittle post function by adding in X and tsq as inputs and updating the documentation for sumX

--- R/whittle_post.R
+++ R/whittle_post.R
@@ -1,8 +1,9 @@
 #' Title
 #'
 #' @param ab  : (K+1) column vector of alpha and beta values
-#' @param sumX : row sums of the X matrix
-#' @param t   : scalar containing tau value
+#' @param X  : basis vector matrix
+#' @param sumX : column sums of the X matrix
+#' @param tsq   : tau squared scalar
 #' @param perio : log of the periodogram
 #' @param sigmasalpha : prior intercept variance, variance associated with the alpha_0 prior
 #'
@@ -10,11 +11,11 @@
 #' @export
 #'
 #' @examples
-whittle_post <- function(ab, sumX, t, perio, sigmasalpha){
+whittle_post <- function(ab, X,sumX, tsq, perio, sigmasalpha){
   # pull out alpha
   a = ab[1]
   # pull out beta
   b = ab[-1]
-  -1/2 * (crossprod(b) / t + a^2 / sigmasalpha + nrow(X) * a + sumX %*% b +
+  -1/2 * (crossprod(b) / tsq + a^2 / sigmasalpha + nrow(X) * a + crossprod(sumX, b) +
             sum(exp(perio - a - X %*% b)))
 }
