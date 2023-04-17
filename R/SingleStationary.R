8752c15bc90c38586d514fdae5e9bc80e5846ff0
llrebecca21 <llrebecca21@gmail.com>
2023-04-17 11:09:36 -0500

    Source the new functions used in the Gaussian Approximation. Change the name of the optim function variable to reflect what the optim function is calculating.

--- R/SingleStationary.R
+++ R/SingleStationary.R
@@ -1,6 +1,8 @@
 # Single Stationary Time Series
 library(mvtnorm)
 set.seed(1080)
+source("R/whittle_post.R")
+source("R/gr_single.R")
 
 # Create single time series: AR(1)
 
@@ -56,8 +58,8 @@ X <- outer(X = omega, Y = 1:K, FUN = function(x,y){sqrt(2/length(omega))*cos(4 *
 round(crossprod(X),5)
 # Specify Sum of X for the posterior function later
 # 1^T_n X Beta part in the paper (excluding the Beta)
-sumX <- crossprod(rep(1, nrow(X)), X)
-
+sumX <- c(crossprod(rep(1, nrow(X)), X))
+length(sumX)
 # Initialize first row of Theta
 Theta[1,] <- c(alpha0, betavalues, tausquared)
 
@@ -70,36 +72,43 @@ perio <- log((abs(fft(ts1)) ^ 2 / n)[1:ceiling(n/2)])
 plot(perio, type = "l")
 length((abs(fft(ts1)) ^ 2 / n))
 
-# Define Posterior Function for Beta and alpha_0
-post_func <- function(b, a, t){
-  exp(-1/2 * (crossprod(b) / t + a^2 / sigmasalpha + nrow(X) * a + sumX %*% b +
-                sum(exp(perio - a - X %*% b))))
-}
-
-
-for (i in 2:iter) {
+for (g in 2:iter) {
+  # g = 2
+  # Extract alpha, beta and tau^2 from theta
+  # alpha:
+  a = Theta[g - 1, 1]
+  # beta:
+  b = Theta[g - 1, -c(1, K+2)]
+  # tau^squared:
+  tsq = Theta[g - 1, K + 2]
   # Metropolis Hastings Step
-  betaprop <- rmvnorm(n = 1, mean = Theta[i - 1, -(K+2)], sigma = 0.03 * diag(K+1))
+  # Maximum A Posteriori (MAP) estimate : finds the alpha and beta that gives us the mode of the conditional posterior
+  map <- optim(par = c(a,b), fn = whittle_post, gr = gr_single, method ="BFGS", control = list(fnscale = -1),
+               X = X, sumX = sumX, tsq = tsq, perio = perio, sigmasalpha = sigmasalpha)
+  
+  # 
+  
+  #betaprop <- rmvnorm(n = 1, mean = Theta[g - 1, -(K+2)], sigma = 0.03 * diag(K+1))
   # calculate acceptance ratio
-  prop_ratio <- min(1, post_func(b = betaprop[-1], a = betaprop[1], t = Theta[i - 1, K + 2]) / post_func(b = Theta[i-1, -c(1, K+2)], a = Theta[i-1, 1], t = Theta[i - 1, K + 2]) )
+  prop_ratio <- min(1, post_func(b = betaprop[-1], a = betaprop[1], t = Theta[i - 1, K + 2]) / post_func(b = Theta[g-1, -c(1, K+2)], a = Theta[g-1, 1], t = Theta[g - 1, K + 2]) )
   # create acceptance decision
   accept <- rbinom(1, 1, prop_ratio)
   if(accept == 1){
     # accept betaprop as new alpha and beta
-    Theta[i, -(K+2)] <- betaprop
+    Theta[g, -(K+2)] <- betaprop
   }else{
-    Theta[i, -(K+2)] <- Theta[i - 1, -(K+2)]
+    Theta[g, -(K+2)] <- Theta[g - 1, -(K+2)]
   }
   # Tau Update: Gibbs Sampler: Inverse CDF sampler
   # truncated gamma
   # draw u first: corresponds to a valid Gamma CDF value
-  u <- runif(1, min = pgamma(q = 1/maxtausquared, shape = K/2 - 1, rate = 1/2 * crossprod(Theta[i,-c(1, K+2)])) , max = 1)
+  u <- runif(1, min = pgamma(q = 1/maxtausquared, shape = K/2 - 1, rate = 1/2 * crossprod(Theta[g,-c(1, K+2)])) , max = 1)
   # Recovering corresponding inverse tau squared
-  invtausquarednew <- qgamma(p = u, shape = K/2 -1, rate = 1/2 * crossprod(Theta[i, -c(1,K+2)]))
+  invtausquarednew <- qgamma(p = u, shape = K/2 -1, rate = 1/2 * crossprod(Theta[g, -c(1,K+2)]))
   # invert to get new tau squared value
   newtau <- 1/invtausquarednew
   # Update Theta matrix with new tau squared value
-  Theta[i,K+2] <- newtau
+  Theta[g,K+2] <- newtau
 }
 
 Theta <- Theta[-(1:500),]
@@ -107,4 +116,14 @@ Theta <- Theta[-(1:500),]
 plot(Theta[,1], type = "l")
 plot(Theta[,2], type = "l")
 plot(Theta[,K+2], type = "l")
+
+
+# Metropolis Hastings Step
+# Bring in the Gaussian Approximation with BFGS Optimization
+# use log to write the acceptance step
+# change index i to g
+
+# Tau Update
+# Change update to half-t distribution
+
 
