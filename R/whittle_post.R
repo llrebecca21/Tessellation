be5126b4fd8b4a0035cdeb2ea3c5ddc8f8fb835c
llrebecca21 <llrebecca21@gmail.com>
2023-05-11 10:43:56 -0500

    Update to include the general D input.

--- R/whittle_post.R
+++ R/whittle_post.R
@@ -1,4 +1,4 @@
-#' Title
+#' Whittle Post
 #'
 #' @param ab  : (K+1) column vector of alpha and beta values
 #' @param X  : basis vector matrix
@@ -6,16 +6,17 @@
 #' @param tsq   : tau squared scalar
 #' @param perio : log of the periodogram
 #' @param sigmasalpha : prior intercept variance, variance associated with the alpha_0 prior
+#' @param D : vector of length K with strictly positive entries
 #'
 #' @return
 #' @export
 #'
 #' @examples
-whittle_post <- function(ab, X,sumX, tsq, perio, sigmasalpha){
+whittle_post <- function(ab, X,sumX, tsq, perio, sigmasalpha, D){
   # pull out alpha
   a = ab[1]
   # pull out beta
   b = ab[-1]
-  -1/2 * (crossprod(b) / tsq + a^2 / sigmasalpha + nrow(X) * a + crossprod(sumX, b) +
+  -1/2 * (sum(b * b / D) / tsq + a^2 / sigmasalpha + nrow(X) * a + crossprod(sumX, b) +
             sum(exp(perio - a - X %*% b)))
 }
