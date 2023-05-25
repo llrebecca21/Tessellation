6ea5af1870a3978709c6ab48f2f98c56ae0455b8
llrebecca21 <llrebecca21@gmail.com>
2023-05-25 15:32:29 -0500

    Create function to calculate the posterior distribution when given multiple time series.

---
+++ R/posterior_multiple.R
@@ -0,0 +1,16 @@
+#' posterior_multiple
+#'
+#' @param b 
+#' @param X 
+#' @param sumX 
+#' @param tsq 
+#' @param perio 
+#' @param D 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+posterior_multiple <- function(b, X, sumX, tsq, perio, D){
+  
+}
