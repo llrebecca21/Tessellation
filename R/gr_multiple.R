05bc8fc7a5cd8f21346612de6ce42f93b9b54e09
llrebecca21 <llrebecca21@gmail.com>
2023-05-25 16:01:28 -0500

    Update so the gradient is calculated for only beta since alpha is no longer seperate from the beta section of theta.

--- R/gr_multiple.R
+++ R/gr_multiple.R
@@ -1,3 +1,18 @@
-gr_multiple <- function(){
-  
+#' Gradient Function for multiple stationary time series with same underlying spectrum
+#'
+#' @param b 
+#' @param X 
+#' @param sumX 
+#' @param tsq 
+#' @param y_bar 
+#' @param D 
+#' @param num_timeseries 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gr_multiple <- function(b, X, sumX, tsq, y_bar, D, num_timeseries){
+  # derivative of log of the whittle_post with respect to beta (vector)
+  derivbeta = -(num_timeseries/2) * (2 * (b / D) / (num_timeseries * tsq) + sumX - colSums(X * c(y_bar / exp(X %*% b))))
 }
