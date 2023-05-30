396106f44f5ef17da6586343c229d8d7741abb9c
llrebecca21 <llrebecca21@gmail.com>
2023-05-30 16:50:35 -0500

    Update the calculation to get rid of unneeded tau^2 multiplication.

--- R/gr_multiple.R
+++ R/gr_multiple.R
@@ -14,5 +14,5 @@
 #' @examples
 gr_multiple <- function(b, X, sumX, tsq, y_bar, D, num_timeseries){
   # derivative of log of the whittle_post with respect to beta (vector)
-  derivbeta = -(num_timeseries/2) * (2 * (b / D) / (num_timeseries * tsq) + sumX - colSums(X * c(y_bar / exp(X %*% b))))
+  derivbeta = -(num_timeseries/2) * (2 * (b / D) / (num_timeseries) + sumX - colSums(X * c(y_bar / exp(X %*% b))))
 }
