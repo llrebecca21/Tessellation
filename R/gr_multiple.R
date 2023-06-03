89bea05b2d167ed547f49ebbfcb02aced8a13324
llrebecca21 <llrebecca21@gmail.com>
2023-06-03 14:31:47 -0500

    Update to get rid of num_timeseries and replace with B.

--- R/gr_multiple.R
+++ R/gr_multiple.R
@@ -6,13 +6,13 @@
 #' @param tsq 
 #' @param y_bar 
 #' @param D 
-#' @param num_timeseries 
+#' @param B 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-gr_multiple <- function(b, X, sumX, tsq, y_bar, D, num_timeseries){
+gr_multiple <- function(b, X, sumX, tsq, y_bar, D, B){
   # derivative of log of the whittle_post with respect to beta (vector)
-  derivbeta = -(num_timeseries/2) * (2 * (b / D) / (num_timeseries) + sumX - colSums(X * c(y_bar / exp(X %*% b))))
+  derivbeta = -(B/2) * (2 * (b / D) / (B) + sumX - colSums(X * c(y_bar / exp(X %*% b))))
 }
