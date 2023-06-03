b44138718dd6680abfee7aa00a1eec02cfe073ad
llrebecca21 <llrebecca21@gmail.com>
2023-06-03 14:32:08 -0500

    Change num_timeseries to B.

--- R/he_multiple.R
+++ R/he_multiple.R
@@ -6,14 +6,14 @@
 #' @param tsq 
 #' @param y_bar 
 #' @param D 
-#' @param num_timeseries 
+#' @param B 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-he_multiple <- function(b, X, sumX, tsq, y_bar, D, num_timeseries){
-  he_bb = (num_timeseries / 2) * crossprod(X , X * c(y_bar / exp(X %*% b)))
+he_multiple <- function(b, X, sumX, y_bar, D, B){
+  he_bb = (B / 2) * crossprod(X , X * c(y_bar / exp(X %*% b)))
   diag(he_bb) =  1 / (D) + diag(he_bb)
   return(he_bb)
 }
