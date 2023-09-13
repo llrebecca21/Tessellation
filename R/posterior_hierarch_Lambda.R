2233bf256f524b237a8aeb9ca8c9844cefb7f576
llrebecca21 <llrebecca21@gmail.com>
2023-09-13 10:12:51 -0500

    Create function to calculate the posterior conditional of beta^(r).

---
+++ R/posterior_hierarch_Lambda.R
@@ -0,0 +1,16 @@
+#' posterior_hierarch_Lambda : 
+#'
+#' @param br 
+#' @param b 
+#' @param Psi 
+#' @param sumPsi 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+posterior_hierarch_Lambda <- function(br, b, Psi, sumPsi, y){
+  -(1/2) * crossprod(br - b, lambda %*% (br - b))  - (sumPsi %*% br) -
+    sum(y / exp(Psi %*% br))
+}
