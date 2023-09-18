f698552173bfcecb6d733234cc3433b8ac8cc2cb
llrebecca21 <llrebecca21@gmail.com>
2023-09-18 13:31:35 -0500

    Create code to plot the posterior mean of the spectral density with the true spectral density for each time series in a single Simulation.

--- R/Hierarch_Simulations_Lambda.R
+++ R/Hierarch_Simulations_Lambda.R
@@ -14,7 +14,7 @@ source("R/arma_spec.R")
 source("R/Model_2/Sim_hierarch_Lambda.R")
 
 # Set outer parameters for simulations
-n = 5000
+n = 1000
 iter = 1000
 R = 8
 B = 10
@@ -68,4 +68,37 @@ for(r in 1:R){
   points(x = omega, y = log(perio_big[1,,r]), col = "green", lwd = 0.5)
   legend("topright", col = c("black", "red"), lwd = c(1,2), legend = c("Estimate", "True"))
 }
+
+
+# Plot the posterior mean log spectral density against the true for a single simulation
+# Grab one of the Simulation
+s = sample(Sim, 1)
+# get true specdens
+par(mfrow = c(1,1))
+true_logspec = log(arma_spec(omega = omega, theta = theta_true_big[s,r]))
+plot(true_logspec, x = omega, type = "l", lwd = 2, col = "red")
+
+# Calculate the posterior mean of the log spectral density of the estimates
+specdens_mean = rowMeans(Psi %*% t(bb_beta_big[s,,,r]))
+lines(specdens_mean, x = omega)
+
+# Create for loop to plot each 
+# Plot the Spectral Density Estimates with True Spectral Density
+par(mfrow = c(2,4))
+for(r in 1:R){
+  specdens = rowMeans(Psi %*% t(bb_beta_big[s,,,r]))
+  plot(x =c(), y=c(), xlim = c(0,3), ylim = c(-4,2), ylab = "Spectral Density", xlab = "omega",
+       main = sprintf("Spectral Density\nr = %g", r))
+  lines(x = omega, y = specdens)
+  lines(x = omega, y = log(arma_spec(omega = omega, theta = theta_true_big[s,r])), col = "red", lwd = 2)
+  #points(x = omega, y = log(perio_big[s,,r]), col = "green", lwd = 0.5)
+  legend("topright", col = c("black", "red"), lwd = c(1,2), legend = c("Post Mean", "True"), bty = "n")
+}
+
+
+
+
+
+
+
 
