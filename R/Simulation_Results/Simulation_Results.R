d0befcddc8190978d4e209daed968509e6d1e0a2
llrebecca21 <llrebecca21@gmail.com>
2023-09-19 10:28:27 -0500

    Create R file that stores the variables used in the Simulation that was run for Model_Single.

---
+++ R/Simulation_Results/Simulation_Results.R
@@ -0,0 +1,14 @@
+# Simulation Variables
+# Set outer parameters for simulations
+# n = 1000
+# iter = 1000
+# R = 8
+# B = 10
+# phi = 0.5
+# # this must match what is used in mcmc_stationary.R
+# burnin = 10
+# Sim = 1000
+# Time difference of 25.26787 mins
+
+
+
