2d2a899e6ddf932f4b3d4139ea7cda8b0deef0dd
llrebecca21 <llrebecca21@gmail.com>
2023-09-19 10:25:36 -0500

    Reorder the way in which the abrupt-abrupt data generation occurs.

--- abrupt_abrupt_simplots.R
+++ abrupt_abrupt_simplots.R
@@ -1,26 +1,35 @@
 # Abrupt-Abrupt Time Series Plots
 
 # First above, second above
+par(mfrow = c(2,3), mar = c(4.5,2.5,3,.2), las = 1)
 ts.plot(x_t[,20])
 ts.plot(x_t[,19])
 ts.plot(x_t[,18])
 ts.plot(x_t[,17])
 ts.plot(x_t[,16])
+mtext("AR(1), phi = 0.9 and phi = -0.7", outer = TRUE, line = -2)
+
 
 # First Above, Second below
-ts.plot(x_t[,15])
-ts.plot(x_t[,14])
-ts.plot(x_t[,13])
-ts.plot(x_t[,12])
+par(mfrow = c(2,3), mar = c(4.5,2.5,3,.2), las = 1)
 ts.plot(x_t[,11])
+ts.plot(x_t[,12])
+ts.plot(x_t[,13])
+ts.plot(x_t[,14])
+ts.plot(x_t[,15])
+mtext("AR(1), phi = -0.3 and phi = 0.5", outer = TRUE, line = -2)
+
 
 # First below, second above
-par(mfrow = c(2,3), mar = c(4.5,2.5,2,.2))
-ts.plot(x_t[,10])
-ts.plot(x_t[,9])
-ts.plot(x_t[,8])
-ts.plot(x_t[,7])
+par(mfrow = c(2,3), mar = c(4.5,2.5,3,.2), las = 1)
 ts.plot(x_t[,6])
+ts.plot(x_t[,7])
+ts.plot(x_t[,8])
+ts.plot(x_t[,9])
+ts.plot(x_t[,10])
+mtext("AR(1), phi = -0.9 and phi = 0.7", outer = TRUE, line = -2)
+
+
 
 
 
