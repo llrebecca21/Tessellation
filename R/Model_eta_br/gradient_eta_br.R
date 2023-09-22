71de7f16a7bebfa42555116efb42fc58729a0952
llrebecca21 <llrebecca21@gmail.com>
2023-09-22 14:30:29 -0500

    Delete a space between the negative sign and the sumPsi at the beginning of the code.

--- R/Model_eta_br/gradient_eta_br.R
+++ R/Model_eta_br/gradient_eta_br.R
@@ -1,3 +1,3 @@
 gradient_eta_br = function(br, sumPsi, Psi, y, Sigma, eta_r){
-  - sumPsi + colSums(Psi * c(y / exp(Psi %*% br))) - 2 * br * eta_r / Sigma
+  -sumPsi + colSums(Psi * c(y / exp(Psi %*% br))) - 2 * br * eta_r / Sigma
 }
