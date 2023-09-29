07e6c6f05e1298184c21bb1f776540baa580f727
llrebecca21 <llrebecca21@gmail.com>
2023-09-29 14:47:38 -0500

    Create function that calculates the hessian of blackboard beta for eta_r model.

--- R/Model_eta_r/he_eta_r.R
+++ R/Model_eta_r/he_eta_r.R
@@ -1,3 +1,5 @@
-he_eta_r = function(){
-  
+he_eta_r = function(Psi, y, br, eta_r, Sigma){
+  he1 = -crossprod(Psi , Psi * c(y / exp(Psi %*% br)))
+  diag(he1) = diag(he1) - 2 * eta_r / Sigma
+  return(he1)
 }
