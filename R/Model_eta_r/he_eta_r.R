0f82845294daf7b83c3305962b4133ed94d89763
llrebecca21 <llrebecca21@gmail.com>
2023-09-29 15:02:31 -0500

    Create documentation for the function that calculates the hessian of this model.

--- R/Model_eta_r/he_eta_r.R
+++ R/Model_eta_r/he_eta_r.R
@@ -1,3 +1,15 @@
+#' function that calculates the hessian of the conditional posterior distribution of beta^r
+#'
+#' @param Psi 
+#' @param y 
+#' @param br 
+#' @param eta_r 
+#' @param Sigma 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 he_eta_r = function(Psi, y, br, eta_r, Sigma){
   he1 = -crossprod(Psi , Psi * c(y / exp(Psi %*% br)))
   diag(he1) = diag(he1) - 2 * eta_r / Sigma
