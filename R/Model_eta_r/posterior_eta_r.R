7070a7659490955b7589c90acfb64719fecc4968
llrebecca21 <llrebecca21@gmail.com>
2023-09-29 15:01:28 -0500

    Add in documentation for the posterior_eta_r function.

--- R/Model_eta_r/posterior_eta_r.R
+++ R/Model_eta_r/posterior_eta_r.R
@@ -1,3 +1,16 @@
+#' Function that calculates the conditional posterior distribution of a single beta^r term
+#'
+#' @param sumPsi 
+#' @param br 
+#' @param Psi 
+#' @param y 
+#' @param eta_r 
+#' @param Sigma 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 posterior_eta_r = function(sumPsi, br, Psi, y, eta_r, Sigma){
   -crossprod(sumPsi, br) - sum(y / exp(Psi %*% br)) - sum(eta_r * br * br / Sigma)
 }
