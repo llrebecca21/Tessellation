65b43c0f36c62be32f831396cc5e9722f69bb3d4
llrebecca21 <llrebecca21@gmail.com>
2023-09-29 14:47:49 -0500

    Create function that calculates the posterior of blackboard beta for eta_r model.

--- R/Model_eta_r/posterior_eta_r.R
+++ R/Model_eta_r/posterior_eta_r.R
@@ -1,3 +1,3 @@
-posterior_eta_r = function(){
-  
+posterior_eta_r = function(sumPsi, br, Psi, y, eta_r, Sigma){
+  -crossprod(sumPsi, br) - sum(y / exp(Psi %*% br)) - sum(eta_r * br * br / Sigma)
 }
