804a48c61b009cbe99981d80f63506c9965fd839
llrebecca21 <llrebecca21@gmail.com>
2023-09-29 15:03:34 -0500

    Create documentation for the function that calculates the gradient of this model.

--- R/Model_eta_r/gradient_eta_r.R
+++ R/Model_eta_r/gradient_eta_r.R
@@ -1,3 +1,16 @@
+#' function that calculates the gradient of the conditional posterior distribution of the beta^r
+#'
+#' @param br 
+#' @param sumPsi 
+#' @param Psi 
+#' @param y 
+#' @param Sigma 
+#' @param eta_r 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gradient_eta_r = function(br, sumPsi, Psi, y, Sigma, eta_r){
   -sumPsi + colSums(Psi * c(y / exp(Psi %*% br))) - 2 * br * eta_r / Sigma
 }
