82fee2675598fda8e6a017137b50d0593cc30b63
llrebecca21 <llrebecca21@gmail.com>
2023-10-22 16:50:48 -0500

    Create a function that calculates the posterior conditional distribution of beta

--- R/Model_Single_diffn/posterior_multiple_n.R
+++ R/Model_Single_diffn/posterior_multiple_n.R
@@ -1,3 +1,7 @@
-posterior_multiple_n = function(){
-  
+posterior_multiple_n = function(b, Psi_list, sumsumPsi ,perio_list, R, Sigma){
+  rs = 0
+  for(r in 1:R){
+    rs = rs + sum(perio_list[[r]] / exp(Psi_list[[r]] %*% b))
+  }
+  return(-1/2 * sum(b * b / Sigma) - sum(sumsumPsi * b) - rs)
 }
