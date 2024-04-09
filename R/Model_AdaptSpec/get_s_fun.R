e39ea6b3d42536db73b39d83f78e4cd45317b7c4
llrebecca21 <llrebecca21@gmail.com>
2024-04-08 19:05:01 -0500

    Update function get_s_fun to reflect the correct checks for death and birth

--- R/Model_AdaptSpec/get_s_fun.R
+++ R/Model_AdaptSpec/get_s_fun.R
@@ -1,21 +1,23 @@
 # Function to determine if birth or death will occur based
 # on properties of the randomly chosen segment S
 
-get_s_fun = function(S, Smax, tmin, timeseries){
+get_s_fun = function(Smax, tmin, timeseries,xi_vec){
+  S = length(xi_vec)-1
+  # check how many current segments contain at least 2*tmin values
+  check_tmin = max(diff(xi_vec)) >= (2*tmin)
   # get current length of the segment S
-  # get the indexing that contains the current segment S
-  len_S = length(timeseries[a,b])
+  len_ts = length(timeseries)
   # Determine if birth can occur
-  can_birth = as.numeric((S < Smax) & (len_S > 2*tmin))
+  can_birth = as.numeric((S < Smax) & check_tmin)
   # Determine if death can occur
-  can_death = as.numeric((S > 0) & (len_S > 2*tmin))
+  can_death = as.numeric(S > 1)
   # Probability of birth
   prob_birth = can_birth/(can_birth + can_death)
   # Probability of death
   prob_death = 1 - prob_birth
   # choice for birth or death (version of only allowing dimension to change by +1 or -1)
-  new_s = s + sample(c(1,-1),1,prob = c(prob_birth,prob_death))
-  return(new_s)
+  new_S = S + sample(c(1,-1),1,prob = c(prob_birth,prob_death))
+  return(new_S)
 }
 
 
