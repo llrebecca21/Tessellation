ba479f819aa8c3bf778734c704fbc7e562b09fbd
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:54:44 -0500

    Remove uneeded -1/2 and change order in which he_bb is calculated

--- R/Model_AdaptSpec/he_adapt.R
+++ R/Model_AdaptSpec/he_adapt.R
@@ -1,7 +1,7 @@
 # Function that calculates the Hessian of the conditional posterior of the beta's
-he_adapt = function(Psi, sumPsi, b, Sigma, perio){
-  he_bb = -0.5 * crossprod(Psi , Psi * c(perio / exp(Psi %*% b)))
-  diag(he_bb) =  -1 / Sigma + diag(he_bb)
+he_adapt = function(beta_mstar, Psi, sumPsi, Sigma, perio){
+  he_bb = -crossprod(Psi , Psi * c(perio / exp(Psi %*% beta_mstar)))
+  diag(he_bb) = diag(he_bb) - 1 / Sigma
   return(he_bb)
 }
 
