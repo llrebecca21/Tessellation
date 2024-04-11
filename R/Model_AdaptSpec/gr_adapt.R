4e1da702e23e5f6fa6c0e557157f344bc5811b2e
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:53:51 -0500

    Change order of the inputs for the function; Change the output to return the vector version of the positive gradient.

--- R/Model_AdaptSpec/gr_adapt.R
+++ R/Model_AdaptSpec/gr_adapt.R
@@ -1,4 +1,4 @@
 # function that calculates the gradient of the conditional posterior of the beta's
-gr_adapt = function(Psi, sumPsi, b, Sigma, perio){
-  return(-0.5 * (c(sumPsi) + 2*b/Sigma - colSums(Psi * c(perio / exp(Psi %*% b)))))
+gr_adapt = function(b, Psi, sumPsi, Sigma, perio){
+  return(c(sumPsi) + b/Sigma - colSums(Psi * c(perio / exp(Psi %*% b))))
 }
