3a1c3c3754714501e12a5014d767aa92f10aca56
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:52:44 -0500

    Change inputs for the function; comment out lines used during debugging steps.

--- R/Model_AdaptSpec/death_fun.R
+++ R/Model_AdaptSpec/death_fun.R
@@ -6,9 +6,11 @@ source("~/Documents/Tessellation/R/Model_AdaptSpec/get_r_fun.R")
 source("~/Documents/Tessellation/R/Model_AdaptSpec/dtau.R")
 source("~/Documents/Tessellation/R/Model_AdaptSpec/rtau.R")
 source("~/Documents/Tessellation/R/Model_AdaptSpec/betapar.R")
-death_fun = function(xi_prop,xi_cur,tmin,Smax,Beta,tau, timeseries, sigmasalpha, D, B, nu_0){
+source("~/Documents/Tessellation/R/Model_AdaptSpec/rxi_death.R")
+death_fun = function(xi_cur,tmin,Smax,Beta,tau, timeseries, sigmasalpha, D, B, nu_0){
   # |xi_prop| < |xi_cur|
-  
+  # get proposal xi
+  xi_prop = rxi_death(xi_cur = xi_cur)
   # q(S^p | S^c) * q(\xi^p | S^c,\xi^c)
   # just the probability death is possible given by the function get_r_fun
   q_seg_xi_death = get_r_fun(Smax = Smax, tmin = tmin, xi_cur = xi_cur, xi_prop = xi_prop)
@@ -16,8 +18,8 @@ death_fun = function(xi_prop,xi_cur,tmin,Smax,Beta,tau, timeseries, sigmasalpha,
   
   # Determine q_tau
   # Propose a tau
-  xi_cur = seq(0,1000,by = 100)
-  xi_prop = c(xi_cur[1:5],xi_cur[7:11])
+  # xi_cur = seq(0,1000,by = 100)
+  # xi_prop = c(xi_cur[1:5],xi_cur[7:11])
   m_star = which(xi_prop != xi_cur[-length(xi_cur)])[1]-1
   
   # extract tau_c1
@@ -39,26 +41,26 @@ death_fun = function(xi_prop,xi_cur,tmin,Smax,Beta,tau, timeseries, sigmasalpha,
   q_tau = dtau(tau_p1 = tau_c1, tau_cur = tau_p)
   
   # extract beta_c1
-  beta_c1 = Beta[,m_star]
+  beta_c1 = Beta[m_star, ]
   # extract beta_c2
-  beta_c2 = Beta[,m_star + 1]
+  beta_c2 = Beta[m_star + 1, ]
 
   # Determine q_beta
-  par_c1 = betapar(tsq = tau_c1, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star-1]+1):(xi_cur[m_star])], B = B)
-  par_c2 = betapar(tsq = tau_c2, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star]+1):(xi_cur[m_star+1])], B = B)
+  par_c1 = betapar(tsq = tau_c1, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star]+1):(xi_cur[m_star + 1])], B = B)
+  par_c2 = betapar(tsq = tau_c2, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star+1]+1):(xi_cur[m_star+2])], B = B)
   
   # get q_betas
-  q_beta1 = mvtnorm::dmvnorm(beta_c1, mean = par_c1$mean, sigma = par_c1$cov)
-  q_beta2 = mvtnorm::dmvnorm(beta_c2, mean = par_c2$mean, sigma = par_c2$cov)
+  q_beta1 = mvtnorm::dmvnorm(beta_c1, mean = par_c1$mean, sigma = par_c1$cov, log = TRUE)
+  q_beta2 = mvtnorm::dmvnorm(beta_c2, mean = par_c2$mean, sigma = par_c2$cov, log = TRUE)
   
   # q_betap
-  par_p = betapar(tsq = tau_p, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star-1]+1):(xi_cur[m_star+1])], B = B)
+  par_p = betapar(tsq = tau_p, sigmasalpha = sigmasalpha, D = D, ts_seg = timeseries[(xi_cur[m_star]+1):(xi_cur[m_star+2])], B = B)
   beta_p = c(mvtnorm::rmvnorm(1, mean = par_p$mean, sigma = par_p$cov))
-  q_betap = mvtnorm::dmvnorm(beta_p, mean = par_p$mean, sigma = par_p$cov)
+  q_betap = mvtnorm::dmvnorm(beta_p, mean = par_p$mean, sigma = par_p$cov, log = TRUE)
   
   # Calculate the Likelihood for birth
   # current:
-  L_c1 = log_likelihood_adapt(b=beta_c1, Psi = par_c1$Psi, sumPsi = par_c1$sumPsi, perio = par_c1$perio) 
+  L_c1 = log_likelihood_adapt(b=beta_c1, Psi = par_c1$Psi, sumPsi = par_c1$sumPsi, perio = par_c1$perio)
   L_c2 = log_likelihood_adapt(b=beta_c2, Psi = par_c2$Psi, sumPsi = par_c2$sumPsi, perio = par_c2$perio)
   
   # proposal:
@@ -89,13 +91,13 @@ death_fun = function(xi_prop,xi_cur,tmin,Smax,Beta,tau, timeseries, sigmasalpha,
   if(U < A){
     # Accept
     if(length(xi_cur) == 3){
-      Beta_prop = matrix(beta_p, nrow = length(beta_p), ncol = 1)
+      Beta_prop = matrix(beta_p, ncol = length(beta_p), nrow = 1)
     }else if(m_star == 1){
-      Beta_prop = cbind(beta_p, Beta[,(m_star+2):ncol(Beta)])
-    } else if(m_star == (ncol(Beta)-1)){
-      Beta_prop = cbind(Beta[,1:(m_star-1)], beta_p)
+      Beta_prop = rbind(beta_p, Beta[(m_star+2):nrow(Beta), ])
+    } else if(m_star == (nrow(Beta)-1)){
+      Beta_prop = rbind(Beta[1:(m_star-1), ], beta_p)
     }else{
-      Beta_prop = cbind(Beta[,1:(m_star-1)], beta_p, Beta[,(m_star+2):ncol(Beta)])
+      Beta_prop = rbind(Beta[1:(m_star-1), ], beta_p, Beta[(m_star+2):nrow(Beta), ])
     }
     return(list("Beta" = Beta_prop, "xi" = xi_prop, "tau" = tau_prop))
   }else{
