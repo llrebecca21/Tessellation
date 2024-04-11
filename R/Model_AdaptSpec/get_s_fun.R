f5ee7b0145a901e2e9b57c45c435f218a0f9ff69
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:50:18 -0500

    Update so function is not dependent on timeseries

--- R/Model_AdaptSpec/get_s_fun.R
+++ R/Model_AdaptSpec/get_s_fun.R
@@ -1,12 +1,12 @@
 # Function to determine if birth or death will occur based
 # on properties of the randomly chosen segment S
 
-get_s_fun = function(Smax, tmin, timeseries,xi_vec){
-  S = length(xi_vec)-1
+get_s_fun = function(Smax, tmin,xi_cur){
+  S = length(xi_cur)-1
   # check how many current segments contain at least 2*tmin values
-  check_tmin = max(diff(xi_vec)) >= (2*tmin)
+  check_tmin = max(diff(xi_cur)) >= (2*tmin)
   # get current length of the segment S
-  len_ts = length(timeseries)
+  len_ts = xi_cur[length(xi_cur)]
   # Determine if birth can occur
   can_birth = as.numeric((S < Smax) & check_tmin)
   # Determine if death can occur
