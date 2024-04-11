8e1ca21622f7732a573e71fc31cf67134e53158e
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:56:12 -0500

    Create function that samples a random xi from the distribution for xi's in the birth step.

---
+++ R/Model_AdaptSpec/rxi_birth.R
@@ -0,0 +1,20 @@
+rxi_birth = function(Smax, tmin, xi_cur){
+  S_cur = length(xi_cur)-1
+  len_s = diff(xi_cur)
+  # check how many current segments contain at least 2*tmin values
+  check_tmin = max(len_s) >= (2*tmin)
+  # Find which segment can be chosen
+  m_star = sample(which(len_s >= 2*tmin),1)
+  # xi_p
+  xi_p = sample((xi_cur[m_star]+tmin):(xi_cur[m_star+1]-tmin),1)
+  # insert xi_p into xi_p
+  xi_prop = append(xi_cur,xi_p,m_star)
+  return(xi_prop)
+}
+
+
+
+
+
+
+
