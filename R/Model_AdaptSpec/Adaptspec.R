ab9c721b5dfe2e443405a1b3bf01304f44ed1c6d
llrebecca21 <llrebecca21@gmail.com>
2024-04-11 10:49:06 -0500

    Add source files for debugging steps; set iter,B, and D as hyperparameters. Create xi_list and theta_list to store outputs of simulation; Create for loop to go over steps in adaptSPEC.

--- R/Model_AdaptSpec/Adaptspec.R
+++ R/Model_AdaptSpec/Adaptspec.R
@@ -15,7 +15,23 @@ source("R/General_Functions/Chol_sampling.R") # performs Cholesky Sampling
 source("R/General_Functions/arma_spec.R") # determines the spectral density of the resulting stationary segment
 source("R/Data_Generation/data_generation.R") # data generating code
 source("R/Sampling_Algorithms/Sampler_Single.R") # Performs the Sampling algorithm for a stationary time series
-
+source("R/Model_AdaptSpec/get_s_fun.R")
+source("R/Model_AdaptSpec/birth_fun.R")
+source("R/Model_AdaptSpec/death_fun.R")
+source("R/Model_AdaptSpec/within_fun.R")
+source("R/Model_AdaptSpec/rxi_death.R")
+source("R/Model_AdaptSpec/betapar.R")
+source("R/Model_AdaptSpec/get_r_fun.R")
+source("R/Model_AdaptSpec/gr_adapt.R")
+source("R/Model_AdaptSpec/he_adapt.R")
+source("R/Model_AdaptSpec/rxi_birth.R")
+source("R/Model_AdaptSpec/dxi_within.R")
+source("R/Model_AdaptSpec/rxi_within.R")
+source("R/Model_AdaptSpec/dxi.R")
+source("R/Model_AdaptSpec/log_likelihood_adapt.R")
+source("R/Model_AdaptSpec/beta_cond_post.R")
+source("R/Model_AdaptSpec/dtau.R")
+source("R/Model_AdaptSpec/rtau.R")
 # # Create a single non-stationary time series
 # set hyper-parameters
 # length of a single time series (entire time-series). Will be prompted in function
@@ -24,6 +40,8 @@ n = 1000
 tmin = 50
 # Determine Smax contstraint. Check will occur in function to make sure Smax \ne 0 or 1
 Smax = floor(n/tmin) 
+# 
+R = 1
 
 # Choose how many known stationary time series segments we generate data for 
 # S_true, either can be prompted in function inputs, or can be randomly generated uniformly from [1,...,Smax]
@@ -64,11 +82,11 @@ timeseries = ts_list[[1]]
 # omega = (2 * pi * (0:J)) / n
 
 # Determine the starting segment break placements \xi_m based on number of segments and tmin condition
-xi_c = c(0,cumsum(s_length[[1]]))
+xi_cur = c(0,cumsum(s_length[[1]]))
 # xi_0 = 0 
-# xi_1 = 264
-# xi_2 = 517
-# xi_3 = 748
+# xi_1 = 261
+# xi_2 = 510
+# xi_3 = 760
 # xi_4 = 1000
 
 # Specify the intervals for the current time series
@@ -77,33 +95,71 @@ xi_c = c(0,cumsum(s_length[[1]]))
 # Segment 3: [518, 748]
 # Segment 4: [749, 1000]
 
+B = 10
+D = 1 / (4 * pi * (1:B)^2)
+
 
 # Determine current number of segments to begin with s^c
 # To begin set it equal to the truth
-S_c = length(xi_c) - 1
-
+S_c = length(xi_cur) - 1
+iter = 100
 # Initialize storage for the basis coefficients
-theta_list = vector(mode = "list", length = 100)
-# Create list to store periodogram
-perio_list = vector(mode = "list", length = 100)
+theta_list = vector(mode = "list", length = iter)
+xi_list = vector(mode = "list", length = iter)
 
 # Run the Sampler_Single on each segment to get the initial values for the basis coefficients
 # create matrix to store the outputs as matrices
-theta_list[[1]] = matrix(NA, nrow = S_c, ncol = B + 3)
-perio_list[[1]] = vector(mode = "list", length = S_c)
+theta_list[[1]] = matrix(NA, nrow = S_c, ncol = B + 2)
 for(k in 1:S_c){
   # Run the sampler on the kth segment of the time-series
-  seg_cur = timeseries[(xi_c[k]+1):(xi_c[k+1])]
+  seg_cur = timeseries[(xi_cur[k] + 1):(xi_cur[k + 1])]
   # run the sampler and store
   init_out = Sampler_Single(cbind(seg_cur))
-  theta_list[[1]][k,] = c(init_out$Theta[nrow(init_out$Theta),], init_out$lambda)
-  perio_list[[1]][[k]] = init_out$perio
+  theta_list[[1]][k,] = init_out$Theta[nrow(init_out$Theta),]
 }
 
-# Set the current Theta and perio as:
-theta_c = theta_list[[1]]
-perio_c = perio_list[[1]]
 
+
+# Set the current Theta and perio as:
+Beta = theta_list[[1]][,-ncol(theta_list[[1]])]
+tau = theta_list[[1]][,ncol(theta_list[[1]])]
+
+for(i in 2:iter){
+  # Get new S (Proposes a birth or death)
+  S_p = get_s_fun(Smax = Smax, tmin = tmin, xi_cur = xi_cur)
+  # Compare S_c with S_p
+  if(S_p > S_c){ # birth move
+    print('DOING BIRTH!')
+    output = birth_fun(xi_cur = xi_cur, tmin = tmin, Smax = Smax, Beta = Beta, tau = tau, timeseries = timeseries, sigmasalpha = 100, D = D, B = B, nu_0 = 3)
+  }else{
+    print('DOING DEATH!')
+    output = death_fun(xi_cur = xi_cur, tmin = tmin, Smax = Smax, Beta = Beta, tau = tau, timeseries = timeseries, sigmasalpha = 100, D = D, B = B, nu_0 = 3)
+  }
+  # Update parameters based on outputs from either the birth or death that was performed
+  # update Beta
+  Beta = output$Beta
+  tau = output$tau
+  xi_cur = output$xi
+  S_c = length(xi_cur)-1
+  
+  
+  # Within model move
+  if(S_c > 1){
+    
+    print("DOING WITHIN!")
+    output = within_fun(xi_cur = xi_cur, tmin = tmin, Smax = Smax, Beta = Beta, tau = tau, timeseries = timeseries, sigmasalpha = 100, D = D, B = B, nu_0 = 3)
+    Beta = output$Beta
+    tau = output$tau
+    xi_cur = output$xi
+    
+  }
+  
+  #output to theta_list
+  theta_list[[i]] = cbind(Beta,tau)
+  #output to xi_list
+  xi_list[[i]] = xi_cur
+
+}
 ###################
 # MCMC Moves
 # contained in a for loop until convergence
