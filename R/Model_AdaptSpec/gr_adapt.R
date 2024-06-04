29423252befd5048e3fefbd437d0f32d77504bbe
llrebecca21 <llrebecca21@gmail.com>
2024-06-04 11:34:49 -0500

    update the call for the gradient to include n

--- R/Model_AdaptSpec/gr_adapt.R
+++ R/Model_AdaptSpec/gr_adapt.R
@@ -1,4 +1,4 @@
 # function that calculates the gradient of the conditional posterior of the beta's
-gr_adapt = function(b, Psi, sumPsi, Sigma, perio){
+gr_adapt = function(b, Psi, sumPsi, Sigma, perio,n){
   return(c(sumPsi) + b/Sigma - colSums(Psi * c(perio / exp(Psi %*% b))))
 }
