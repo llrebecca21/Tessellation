93037ea36c98271c5474ef56340ac5f65082b549
llrebecca21 <llrebecca21@gmail.com>
2024-06-04 11:40:23 -0500

    Fix the dimension issue in the posterior function of the eta_br model.

--- R/Model_eta_br/posterior_eta_br.R
+++ R/Model_eta_br/posterior_eta_br.R
@@ -1,3 +1,3 @@
 posterior_eta_br = function(sumPsi, br, Psi, y, eta_r, Sigma){
-  -crossprod(sumPsi, br) - sum(y / exp(Psi %*% br)) - sum(eta_r * br * br / Sigma)
+  -crossprod(sumPsi, br) - sum(y / exp(Psi %*% br)) - sum(c(1,eta_r) * br * br / Sigma)
 }
