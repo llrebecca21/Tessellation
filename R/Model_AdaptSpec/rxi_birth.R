387a89e55793176b70ee318bfd062ed6fea3c15f
llrebecca21 <llrebecca21@gmail.com>
2024-06-04 11:36:01 -0500

    Change how the indexing is stored for the updates of xi proposal and m_star.

--- R/Model_AdaptSpec/rxi_birth.R
+++ R/Model_AdaptSpec/rxi_birth.R
@@ -4,9 +4,11 @@ rxi_birth = function(Smax, tmin, xi_cur){
   # check how many current segments contain at least 2*tmin values
   check_tmin = max(len_s) >= (2*tmin)
   # Find which segment can be chosen
-  m_star = sample(which(len_s >= 2*tmin),1)
+  w = which(len_s >= 2*tmin)
+  m_star = w[sample(1:length(w),1)]
   # xi_p
-  xi_p = sample((xi_cur[m_star]+tmin):(xi_cur[m_star+1]-tmin),1)
+  i = (xi_cur[m_star]+tmin):(xi_cur[m_star+1]-tmin)
+  xi_p = i[sample(length(i),1)]
   # insert xi_p into xi_p
   xi_prop = append(xi_cur,xi_p,m_star)
   return(xi_prop)
