94264a776ac39b0fbc8374a67cb497af17e5e312
llrebecca21 <llrebecca21@gmail.com>
2024-06-04 11:35:24 -0500

    Add in the constant term to the calculation to better match the math.

--- R/Model_AdaptSpec/log_likelihood_adapt.R
+++ R/Model_AdaptSpec/log_likelihood_adapt.R
@@ -1,3 +1,3 @@
-log_likelihood_adapt = function(b, Psi,sumPsi, perio){
-  return(-1/2 * (sum(sumPsi*b) + sum(perio/exp(Psi %*% b))))
+log_likelihood_adapt = function(b, Psi,sumPsi, perio,n){
+  return(-1/2 * (sum(sumPsi*b) + sum(perio/exp(Psi %*% b))) - (n/2)*log(2*pi))
 }
