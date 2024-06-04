58266b3519884c7a6dbf6b7e374346d96dfac838
llrebecca21 <llrebecca21@gmail.com>
2024-06-04 11:30:35 -0500

    Add the constant term into the conditional posterior of the betas function.

--- R/Model_AdaptSpec/beta_cond_post.R
+++ R/Model_AdaptSpec/beta_cond_post.R
@@ -1,3 +1,4 @@
-beta_cond_post = function(b, Psi,sumPsi, perio, Sigma){
-  1/2 * sum(b * b / Sigma) + sum(sumPsi*b) + sum(perio/exp(Psi %*% b))
+beta_cond_post = function(b, Psi,sumPsi, perio, Sigma,n){
+  # This is in log scale already
+  1/2 * sum(b * b / Sigma) + sum(sumPsi*b) + sum(perio/exp(Psi %*% b)) + (n/2)*log(2*pi)
 }
