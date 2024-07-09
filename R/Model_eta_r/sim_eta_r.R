16986ebb10450b0a6aa7e67eb063c10d4ab9e647
llrebecca21 <llrebecca21@gmail.com>
2024-07-08 20:21:10 -0500

    Change inputs to function call of Sampler to match new variable input names

--- R/Model_eta_r/sim_eta_r.R
+++ R/Model_eta_r/sim_eta_r.R
@@ -1,11 +1,7 @@
 # Create a function of the eta_r model for simulations for single or multiple stationary time series
 
-sim_eta_r = function(n,iter,phi,R,B){
+sim_eta_r = function(n,iter,phi,R,B,nu,etasq,tausquared,lambda,theta_z,nu_r,burnin, scaling){
   # check the correct values are fed into the Sampler
-  # print(iter)
-  # print(phi)
-  # print(R)
-  # print(B)
 
   # Call function to generate data
   data_gen = generate_adapt(phi = phi, n = n, R = R)
@@ -13,7 +9,7 @@ sim_eta_r = function(n,iter,phi,R,B){
   timeseries = data_gen$matrix_timeseries
   
   # call Sampler_eta_r
-  return(Sampler_eta_r(timeseries, B, iter, nu = 10, etasq = 1, tausquared = 1, lambda = 1))
+  return(Sampler_eta_r(timeseries, B, iter, nu = 10, etasq = 1, tausquared = 1, lambda = 1, theta_z = 1, nu_r = 1, burnin = burnin, scaling = 1))
 }
 
 
